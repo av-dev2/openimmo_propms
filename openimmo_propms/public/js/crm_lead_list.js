35ef58fcadb283d3ca96f97d23aeecb7654e36d5
av-dev2 <mgani@aakvatech.com>
2026-09-09 17:06:43 +0300

    style: format public/js/crm_lead_list.js with prettier

--- openimmo_propms/public/js/crm_lead_list.js
+++ openimmo_propms/public/js/crm_lead_list.js
@@ -1,49 +1,52 @@
-frappe.listview_settings['CRM Lead'] = {
-	refresh: function(listview) {
-		listview.page.add_inner_button(__('Import Lead XML'), function() {
+frappe.listview_settings["CRM Lead"] = {
+	refresh: function (listview) {
+		listview.page.add_inner_button(__("Import Lead XML"), function () {
 			new frappe.ui.Dialog({
-				title: __('Import Lead XML'),
+				title: __("Import Lead XML"),
 				fields: [
 					{
-						label: __('Manual Source'),
-						fieldname: 'source_name',
-						fieldtype: 'Link',
-						options: 'Integration Source',
-						get_query: function() {
+						label: __("Manual Source"),
+						fieldname: "source_name",
+						fieldtype: "Link",
+						options: "Integration Source",
+						get_query: function () {
 							return {
 								filters: {
-									'source_type': 'Manual Upload',
-									'enabled': 1
-								}
-							}
+									source_type: "Manual Upload",
+									enabled: 1,
+								},
+							};
 						},
-						reqd: 1
+						reqd: 1,
 					},
 					{
-						label: __('XML File'),
-						fieldname: 'xml_file',
-						fieldtype: 'Attach',
-						reqd: 1
-					}
+						label: __("XML File"),
+						fieldname: "xml_file",
+						fieldtype: "Attach",
+						reqd: 1,
+					},
 				],
-				primary_action_label: __('Import'),
+				primary_action_label: __("Import"),
 				primary_action(values) {
 					frappe.call({
-						method: 'openimmo_propms.api.server_script.import_lead_xml',
+						method: "openimmo_propms.api.server_script.import_lead_xml",
 						args: {
 							file_url: values.xml_file,
-							source_name: values.source_name
+							source_name: values.source_name,
 						},
-						callback: function(r) {
+						callback: function (r) {
 							if (!r.exc) {
-								frappe.show_alert({message: __('Imported successfully'), indicator: 'green'});
+								frappe.show_alert({
+									message: __("Imported successfully"),
+									indicator: "green",
+								});
 								listview.refresh();
 							}
-						}
+						},
 					});
 					this.hide();
-				}
+				},
 			}).show();
 		});
-	}
+	},
 };
