5f5c63ca087ef69f4dd882c814abfe5e9f5eb2de
av-dev2 <mgani@aakvatech.com>
2026-09-09 17:04:42 +0300

    style: format test_integration_source.py with ruff

--- openimmo_propms/openimmo_propms/doctype/integration_source/test_integration_source.py
+++ openimmo_propms/openimmo_propms/doctype/integration_source/test_integration_source.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2025, Talib sheikh and Contributors
 # See license.txt
 
-from types import SimpleNamespace
 import xml.etree.ElementTree as ET
+from types import SimpleNamespace
 
 from frappe.tests.utils import FrappeTestCase
 
@@ -10,73 +10,73 @@ from openimmo_propms.services.export_engine import _append_image_attachment, _re
 
 
 class TestIntegrationSource(FrappeTestCase):
-    def test_append_image_attachment_uses_main_child_parent_and_fallback_order(self):
-        source = SimpleNamespace(
-            image_field="main_picture",
-            child_image_field="child_pictures.image",
-            parent_image_field="parent_pictures.image",
-            fallback_image_field="property_type_symbol",
-            image_group="TITELBILD",
-            image_location="EXTERN",
-            base_media_url="https://example.com",
-            target_doctype="Property",
-            field_mappings=[],
-        )
-        record = {
-            "main_picture": "/files/main.jpg",
-            "child_pictures": [{"image": "/files/child-1.jpg"}, {"image": "/files/child-2.jpg"}],
-            "parent_pictures": [{"image": "/files/parent-1.jpg"}],
-            "property_type_symbol": "/files/symbol.jpg",
-        }
-        immobilie = ET.Element("immobilie")
+	def test_append_image_attachment_uses_main_child_parent_and_fallback_order(self):
+		source = SimpleNamespace(
+			image_field="main_picture",
+			child_image_field="child_pictures.image",
+			parent_image_field="parent_pictures.image",
+			fallback_image_field="property_type_symbol",
+			image_group="TITELBILD",
+			image_location="EXTERN",
+			base_media_url="https://example.com",
+			target_doctype="Property",
+			field_mappings=[],
+		)
+		record = {
+			"main_picture": "/files/main.jpg",
+			"child_pictures": [{"image": "/files/child-1.jpg"}, {"image": "/files/child-2.jpg"}],
+			"parent_pictures": [{"image": "/files/parent-1.jpg"}],
+			"property_type_symbol": "/files/symbol.jpg",
+		}
+		immobilie = ET.Element("immobilie")
 
-        _append_image_attachment(source, record, immobilie)
+		_append_image_attachment(source, record, immobilie)
 
-        attachments = immobilie.findall("./anhaenge/anhang")
-        self.assertEqual(len(attachments), 4)
-        self.assertEqual(
-            [attachment.findtext("daten") for attachment in attachments],
-            [
-                "https://example.com/files/main.jpg",
-                "https://example.com/files/child-1.jpg",
-                "https://example.com/files/child-2.jpg",
-                "https://example.com/files/parent-1.jpg",
-            ],
-        )
+		attachments = immobilie.findall("./anhaenge/anhang")
+		self.assertEqual(len(attachments), 4)
+		self.assertEqual(
+			[attachment.findtext("daten") for attachment in attachments],
+			[
+				"https://example.com/files/main.jpg",
+				"https://example.com/files/child-1.jpg",
+				"https://example.com/files/child-2.jpg",
+				"https://example.com/files/parent-1.jpg",
+			],
+		)
 
-    def test_append_image_attachment_uses_fallback_when_no_other_images_exist(self):
-        source = SimpleNamespace(
-            image_field="main_picture",
-            child_image_field="child_pictures.image",
-            parent_image_field="parent_pictures.image",
-            fallback_image_field="property_type_symbol",
-            image_group="TITELBILD",
-            image_location="EXTERN",
-            base_media_url="https://example.com",
-            target_doctype="Property",
-            field_mappings=[],
-        )
-        record = {
-            "main_picture": None,
-            "child_pictures": [],
-            "parent_pictures": [],
-            "property_type_symbol": "/files/symbol.jpg",
-        }
-        immobilie = ET.Element("immobilie")
+	def test_append_image_attachment_uses_fallback_when_no_other_images_exist(self):
+		source = SimpleNamespace(
+			image_field="main_picture",
+			child_image_field="child_pictures.image",
+			parent_image_field="parent_pictures.image",
+			fallback_image_field="property_type_symbol",
+			image_group="TITELBILD",
+			image_location="EXTERN",
+			base_media_url="https://example.com",
+			target_doctype="Property",
+			field_mappings=[],
+		)
+		record = {
+			"main_picture": None,
+			"child_pictures": [],
+			"parent_pictures": [],
+			"property_type_symbol": "/files/symbol.jpg",
+		}
+		immobilie = ET.Element("immobilie")
 
-        _append_image_attachment(source, record, immobilie)
+		_append_image_attachment(source, record, immobilie)
 
-        attachments = immobilie.findall("./anhaenge/anhang")
-        self.assertEqual(len(attachments), 1)
-        self.assertEqual(attachments[0].findtext("daten"), "https://example.com/files/symbol.jpg")
+		attachments = immobilie.findall("./anhaenge/anhang")
+		self.assertEqual(len(attachments), 1)
+		self.assertEqual(attachments[0].findtext("daten"), "https://example.com/files/symbol.jpg")
 
-    def test_requires_full_doc_for_dotted_image_fields(self):
-        source = SimpleNamespace(
-            image_field="main_picture",
-            child_image_field="child_pictures.image",
-            parent_image_field=None,
-            fallback_image_field=None,
-            field_mappings=[],
-        )
+	def test_requires_full_doc_for_dotted_image_fields(self):
+		source = SimpleNamespace(
+			image_field="main_picture",
+			child_image_field="child_pictures.image",
+			parent_image_field=None,
+			fallback_image_field=None,
+			field_mappings=[],
+		)
 
-        self.assertTrue(_requires_full_doc(source))
+		self.assertTrue(_requires_full_doc(source))
